42660164645367c6e265e61b832dd6992b9baa40
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-10 17:59:38 +0300

    feat: add hook useDebounce

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react"
-import List from "./components/List"
-
+import React, {useState} from "react"
+import useDebounce from "./hooks/useDebounce";
 
 function App() {
+
+  const [value, setValue] = useState('')
+  const debouncedSearch = useDebounce(search, 500)
+
+  function search(query) {
+    fetch(`https://jsonplaceholder.typicode.com/todos?query=${query}`)
+        .then(response => response.json())
+        .then(json => {
+            console.log('json', json);
+        })
+  }
+
+  const onChange = (e) => {
+    // e.preventDefault()
+    setValue(e.target.value)
+    debouncedSearch(e.target.value)
+  }
+
   return (
     <div>
-      <List />
+      <input type={'text'} value={value} onChange={onChange} />
     </div>
   )
 }
