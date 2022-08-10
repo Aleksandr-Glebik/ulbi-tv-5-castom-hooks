d12445a3ebdb34200a6e9a1e7a15473e54353c9f
Aleksandr-Glebik <glebikaa@mail.ru>
2022-08-10 17:25:30 +0300

    feat: add hook useScroll

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import React, { useRef } from "react"
-import Hover from "./components/Hover"
-import useHover from "./hooks/useHover"
+import React from "react"
+import List from "./components/List"
+
 
 function App() {
-  const ref = useRef()
-  const isBlackHovering = useHover(ref)
-
   return (
     <div>
-      <Hover />
-      <div ref={ref}
-         style={{
-           width: 200,
-           height: 200,
-           background: isBlackHovering ? 'black' : 'gray'
-         }}>
-      </div>
+      <List />
     </div>
   )
 }
